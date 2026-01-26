fdb378dbe17949f7b1abe888aed88ce0558f567e
Sesh <sergeimorozov212001@gmail.com>
2026-01-26 13:29:27 +0300

    deploy()

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,11 +1,11 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
-import {APPWRITE_API_V1, PROJECT_ID} from "@/shared/const.ts";
+import {APPWRITE_ACCOUNT_API_V1, PROJECT_ID} from "@/shared/const.ts";
 import type {SignInData} from "@/shared/user/types.ts";
 
 export const authApi = createApi({
   reducerPath: 'authApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: APPWRITE_API_V1,
+    baseUrl: APPWRITE_ACCOUNT_API_V1,
     prepareHeaders: (headers) => {
       headers.set('X-Appwrite-Project', PROJECT_ID);
       return headers;
@@ -14,7 +14,7 @@ export const authApi = createApi({
   endpoints: (builder) => ({
     auth: builder.mutation<void, SignInData>({
       query: (signin) => ({
-        url: `/account/sessions/email`,
+        url: `/sessions/email`,
         method: 'POST',
         body: signin,
       })
